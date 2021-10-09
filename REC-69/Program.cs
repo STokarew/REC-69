214eb9ce9ba608cc8f356f3ebf6b5511f2715bff
STokarew <Sseregatokarew@gmail.com>
2021-10-09 14:36:35 +0700

    Добавьте файлы проекта.

---
+++ REC-69/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace REC_69
+{
+    class Program
+    {
+        static double GetSr( int x)
+        {
+            return Math.Round((Math.PI*Math.Pow(x,2)),2);
+        }
+        static double GetSd(int x)
+        {
+            return Math.Round(((Math.PI * Math.Pow(x, 2)) /4),2);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус круга: ");
+            int r = Convert.ToInt32(Console.ReadLine());
+            double S = GetSr(r);
+            Console.WriteLine($"Площадь круга равна: {S}см") ;
+
+
+            Console.Write("Введите диамерт круга: ");
+            int d = Convert.ToInt32(Console.ReadLine());
+            double S1 = GetSd(d);
+            Console.WriteLine($"Площадь круга равна: {S1}см");
+
+            Console.ReadKey();
+        }
+    }
+}
